a2a5d0ada1720c0bf8374f77e6635d2a6754927d
ganesh-krisartech <ganesh@krisar.tech>
2025-08-20 14:58:45 +0530

    v2 changes

--- scripts/my_script.py
+++ scripts/my_script.py
@@ -3,10 +3,19 @@ import os
 # Access the environment variable
 my_variable_value = os.environ.get("MY_VARIABLE")
 login_time = os.environ.get("LOGIN_TIME")
+first_name = os.environ.get("FIRST_NAME")
+final_date = os.environ.get("FINAL_DATE")
 
+
+print(f"The login time is: {login_time}")
+print(f"The first name is: {first_name}")
+print(f"The login date is: {final_date}")
 
 if my_variable_value:
     print(f"The variable from GitHub Actions is: {my_variable_value}")
     print(f"The login time is: {login_time}")
+    print(f"The first name is: {first_name}")
+    print(f"The login date is: {final_date}")
+
 else:
     print("MY_VARIABLE not found in environment.")
